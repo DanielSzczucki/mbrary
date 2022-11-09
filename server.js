09e1250c06fdc57f48ca96ded6474c89790f3280
DanielSzczucki <117800247+DanielSzczucki@users.noreply.github.com>
2022-11-09 20:00:55 +0100

    Initial setup

---
+++ server.js
@@ -0,0 +1,35 @@
+//sprawdzamy, jeśli nie odpali sie w srodowisku produkcyjnymn,wtedy załąduje bibliotekę dotenv
+
+const DATABASE_URL = "mongodb://127.0.0.1:27017/mybrary";
+
+const express = require("express");
+const app = express();
+const expressLayouts = require("express-ejs-layouts");
+const mongoClient = require("mongodb");
+
+const indexRouter = require("./routes/index");
+
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout");
+app.use(expressLayouts);
+app.use(express.static("public"));
+
+//pobranie repo bazy danych
+const { mongoose } = require("mongoose");
+//ustanowienie zmiennej do nawiązania połaczenia
+mongoose.connect(DATABASE_URL, {
+  useNewUrlParser: true,
+});
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+//pojedyńcze połaczenie do db
+db.once("open", () => {
+  console.log("Connected to db");
+});
+
+app.use("/", indexRouter);
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Listetning on http://localhost:3000");
+});
