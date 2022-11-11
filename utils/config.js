4fcb3ec53e73c8c6f15dcf9d3005e60ca7f7b247
DanielSzczucki <117800247+DanielSzczucki@users.noreply.github.com>
2022-11-11 19:34:18 +0100

    db access changes

---
+++ utils/config.js
@@ -0,0 +1,7 @@
+const { config } = require("dotenv");
+
+module.exports = {
+  db: "mongodb+srv://daniel:ZaAWyI6qJdF3u1R5@samuraj.vnd9qmc.mongodb.net/?retryWrites=true&w=majority",
+  keySession: ["TWOJKLUCZ"],
+  maxAgeSession: 24 * 60 * 60 * 1000,
+};
