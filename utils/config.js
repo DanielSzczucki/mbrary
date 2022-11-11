e04a81a447e733d5171fd6d6190dc8276551ea39
DanielSzczucki <117800247+DanielSzczucki@users.noreply.github.com>
2022-11-11 19:44:02 +0100

    add db changes

--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,3 @@
-const { config } = require("dotenv");
-
 module.exports = {
   db: "mongodb+srv://daniel:ZaAWyI6qJdF3u1R5@samuraj.vnd9qmc.mongodb.net/?retryWrites=true&w=majority",
   keySession: ["TWOJKLUCZ"],
